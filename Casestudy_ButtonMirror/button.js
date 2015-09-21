ca710f7e03a548d90d21be842bb4c0d7271712ba
avseoul <av.seoul@gmail.com>
2015-09-21 02:57:54 -0400

    .

--- Casestudy_ButtonMirror/button.js
+++ Casestudy_ButtonMirror/button.js
@@ -1,31 +1,32 @@
 var btn = function(_x, _y){
-	this.backColor = new p5.Vector(52., 100., 157.);
-	this.backColorB = new p5.Vector(52., 100., 157.);
+	this.backColor = new p5.Vector(77., 216., 101.);
+	this.backColorB = new p5.Vector(77., 216., 101.);
 	// this.backColorR = new p5.Vector(205., 107., 106.);
-	this.backColorR = new p5.Vector(205., 205., 205.);
+	// this.backColorR = new p5.Vector(255., 83., 90.);
+	this.backColorR = new p5.Vector(255., 255., 255.);
 	this.backSize = new p5.Vector(26., 18.);
 	this.backRadius = 100.;
 
-	this.position = new p5.Vector(_x * this.backSize.x, _y * this.backSize.y);
+	this.position = new p5.Vector(_x * this.backSize.x + 1.*_x, _y * this.backSize.y + 1.*_y);
 
-	this.btnRadius = 14.;
-	this.btnPosition = new p5.Vector(this.btnRadius * .65, this.backSize.y * .5 + 1.5);
-	this.btnPositionL = this.btnRadius * .65;
-	this.btnPositionR = this.btnRadius * 1.4;
+	this.btnRadius = 16.;
+	this.btnRTarget = 17.;
+	this.btnRRoot = 16.;
+	this.btnPosition = new p5.Vector(this.btnRadius * .65, this.backSize.y * .5 + 1.0);
+	this.btnPositionL = new p5.Vector(this.btnRadius * .65, this.backSize.y * .5 + 1.5);
+	this.btnPositionR = new p5.Vector(this.btnRadius * 1.1, this.backSize.y * .5 + 1.0);
 
-	this.isHit = false;
+	this.isHit = true;
 
 };
 
 btn.prototype.update = function(){
 	if(this.isHit == true){
 		this.backColor = this.backColorB;
-		this.btnPosition.x = this.btnPositionR;
-
-		// console.log('isHit');
+		this.btnPosition = this.btnPositionR;
 	} else {
 		this.backColor = this.backColorR;
-		this.btnPosition.x = this.btnPositionL;
+		this.btnPosition = this.btnPositionL;
 	}
 };
 
@@ -34,16 +35,28 @@ btn.prototype.draw = function(){
 	{
 		translate(this.position.x, this.position.y);
 
-		strokeWeight(1.);
+		strokeWeight(1);
 
 		//draw btn background
-		fill(this.backColor.x, this.backColor.y, this.backColor.z);
-		stroke(255.);
+		if(this.isHit == true){
+			noStroke();
+			fill(this.backColor.x, this.backColor.y, this.backColor.z);
+		} else {
+			stroke(220.);
+			fill(250);
+		}
+		
 		rect(1, 1, this.backSize.x, this.backSize.y, this.backRadius);
 
 		//draw btn button 
+		if(this.isHit == true){
+			noStroke();
+			this.btnRadius = this.btnRRoot;
+		} else {
+			stroke(230.);
+			this.btnRadius = this.btnRTarget;
+		}
 		fill(255.);
-		stroke(235.);
 		ellipse(this.btnPosition.x, this.btnPosition.y, this.btnRadius, this.btnRadius);
 	}
 	pop();
