7cc0c9ff54fd09f21792e617d77bbe55dbfe2179
avseoul <av.seoul@gmail.com>
2015-09-26 00:13:34 -0400

    .

--- Casestudy_PA2Z_W02/framework.js
+++ Casestudy_PA2Z_W02/framework.js
@@ -1,5 +1,4 @@
-var dropZone, input, button, sample, clearButton;
-var paragraphs = [];
+var input, button, sample;
 
 function setup() {
   noCanvas();
@@ -9,8 +8,10 @@ function setup() {
   // Selecting the text field and button
   input = select('#textinput');
   button = select('#submit');
+  sample = select('#sample');
   // What to do when button pressed
   button.mousePressed(handleInput);
+  sample.mousePressed(getSample);
 }
 
 //get sample text
@@ -25,21 +26,68 @@ function fileLoaded(data) {
 
 // Handle the text input field
 function handleInput() {
-  //reset first
-  var reset = document.getElementById('eventWindow');
-  while (reset.firstChild) {
-    reset.removeChild(reset.firstChild);
-  }
-
-  //evaluate
   process(input.value());
 }
 
-// Processing the text in this example isn't anything
-// Just spitting it back out in the window
 function process(txt) {
-  var node = document.createElement('P');
-  var textnode = document.createTextNode(txt);         
-  node.appendChild(textnode);                  
-  document.getElementById('eventWindow').appendChild(node);
+  //reset first
+  var reset_01 = document.getElementById('eventWindow');
+  var reset_02 = document.getElementById('counter');
+  while (reset_01.firstChild) {
+    reset_01.removeChild(reset_01.firstChild);
+  }
+  while (reset_02.firstChild) {
+    reset_02.removeChild(reset_02.firstChild);
+  }
+  
+  var newParagraph = document.createElement('P');
+
+  var words = splitTokens(txt, ' .,:;!@#$%&*()\n'); 
+  var counter = 0;
+  for (var i = 0; i < words.length; i++) {
+    var newDiv = document.createElement('div');
+    newDiv.style.display = 'inline';  
+
+    //detect fuck
+    var fuck = words[i].match(/fuck/gi);
+
+    //if there is fuck
+    if(fuck){
+      //count fuck
+      counter++;
+
+      var indexDiv = document.createElement('div');
+      indexDiv.style.display = 'inline';  
+      var indexnode = document.createTextNode(counter + '. ');
+      indexDiv.appendChild(indexnode);
+      
+      var textnode = document.createTextNode(words[i] + ' ');
+      newDiv.appendChild(textnode);
+
+      //decorate fuck
+      newDiv.id = 'isFucked';
+      indexDiv.id = 'isIndex';
+
+      newParagraph.appendChild(indexDiv);  
+      newParagraph.appendChild(newDiv);
+    } else {
+      var textnode = document.createTextNode(words[i] + ' ');
+      newDiv.appendChild(textnode);
+
+      newParagraph.appendChild(newDiv); 
+    }                  
+  }
+  //add message
+  var newMessage = document.createTextNode('There are ' + counter + ' fucks down there.' );
+  document.getElementById('counter').appendChild(newMessage);
+  //add fucks 
+  document.getElementById('eventWindow').appendChild(newParagraph);
+
+  // Temporarily overriding splitTokens until
+  // https://github.com/processing/p5.js/pull/931
+  // made by Daniel Shiffman
+  function splitTokens() {
+    var d = (arguments.length > 1) ? new RegExp('[' + arguments[1] + ']', 'g') : /\s/g;
+    return arguments[0].split(d).filter(function(n){return n;});
+  };
 }
