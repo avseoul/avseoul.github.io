03207624b5dccca38f9d07b92c7e1fdfc927438b
avseoul <av.seoul@gmail.com>
2016-01-09 18:30:57 -0500

    .

--- particleEqualizer/src/mainPage.js
+++ particleEqualizer/src/mainPage.js
@@ -1,6 +1,6 @@
 SC.initialize({
   client_id: '1c78c29753054d2e9af8926dd4a77db3',
-  redirect_uri: 'src/callback.html'
+  redirect_uri: 'https://avseoul.github.io/particleEqualizer/src/callback'
 });
 
 // initiate auth popup
