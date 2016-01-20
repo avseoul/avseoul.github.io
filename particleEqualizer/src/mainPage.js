c6cb700449268d6a3fb52ec135a2640e7f93cc4e
avseoul <av.seoul@gmail.com>
2016-01-20 02:30:30 -0500

    .

--- particleEqualizer/src/mainPage.js
+++ particleEqualizer/src/mainPage.js
@@ -2,17 +2,54 @@ var isMobile = false;
 (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))isMobile = true})(navigator.userAgent||navigator.vendor||window.opera);
 
 /* check framedrops */
+var isLow = false;
 var resume = function(){
-    audioNode.mediaElement.play();
     isLow = false;
 
     var b = document.getElementById('isLow_bg');
     document.body.removeChild(b);
     var c = document.getElementById('isLow_container');
     document.body.removeChild(c);
+
+    audioNode.mediaElement.play();
+    isPlaying = true;
 };
-var isLow = false;
+var lfnt_bg_opacity = 0;
+var lfnt_ct_opacity = 0;
+var lfnt_boolean = false;
+var lowFrameNotiTransition = function(){
+    if(!lfnt_boolean){
+        requestAnimationFrame( lowFrameNotiTransition );
+    }
+    var bg = document.getElementById('isLow_bg');
+    bg.style['opacity'] = lfnt_bg_opacity.toString();
+    var container = document.getElementById('isLow_container');
+    container.style['opacity'] = lfnt_ct_opacity.toString();
+    var pos = (1-lfnt_ct_opacity/.8) * 100; 
+    container.style['top'] = pos.toString();
+    var vol = 1-lfnt_ct_opacity/.8;
+    mAudio.volume = vol;
+    lfnt_bg_opacity += .04;
+    var t = .8-lfnt_ct_opacity;
+    lfnt_ct_opacity += t*.1;
+    if(lfnt_bg_opacity > .5){
+        lfnt_bg_opacity = .5;
+    }   
+    if(lfnt_ct_opacity > .79){
+        lfnt_ct_opacity = .8;
+        audioNode.mediaElement.pause();
+        mAudio.volume = 1;
+        lfnt_boolean = true;
+    }
+};
 var lowFrameNoti = function(){
+    var bg = document.getElementById('isLow_bg');
+    bg.style['z-index'] = '1999';
+    var container = document.getElementById('isLow_container');
+    container.style['z-index'] = '2000';
+    lowFrameNotiTransition();
+};
+var preLoadlowFrameNoti = function(){
     var bg = document.createElement('div');
     bg.id = 'isLow_bg';
     bg.addEventListener('click', resume);
@@ -25,39 +62,42 @@ var lowFrameNoti = function(){
     bg.style['left'] = '0';
     bg.style['right'] = '0';
     bg.style['margin'] = 'auto';
-    bg.style['z-index'] = '1999';
+    bg.style['z-index'] = '-1';
     bg.style['opacity'] = '.5';
     var container = document.createElement('div');
     container.id = 'isLow_container';
     container.style['width'] = '400px';
+    container.style['height'] = '600px';
     container.style['background'] = '#fff';
     container.style['position'] = 'absolute';
-    container.style['top'] = '0';
+    container.style['top'] = '100';
     container.style['bottom'] = '0';
     container.style['left'] = '0';
     container.style['right'] = '0';
     container.style['margin'] = 'auto';
-    container.style['z-index'] = '2000';
+    container.style['z-index'] = '-1';
     container.style['opacity'] = '.8';
 
     var notification_a = document.createElement('div');
     notification_a.style['font-size'] = '11px';
     notification_a.style['padding'] = '5px';
     notification_a.innerHTML = 
-        'I\'m sorry,<br>'+
-        'Unfortunately, your computer does not fully support Particle Equalizer #1.<br><br>'+ 
+        '<br>I\'m sorry,<br><br>'+
+        'Unfortunately, your computer does not fully support Particle Equalizer #1.'+ 
         'You can try it with low frame rates by ignoring this message'+
-        ' or check out the video instead.';
+        ' or check out the video instead.'+
+        '<p>&#x20;</p>';
     container.appendChild(notification_a);
 
     var videoPlayer = document.createElement('div');
-    videoPlayer.innerHTML = '<iframe src="https://player.vimeo.com/video/152375642" width="400" height="255" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+    videoPlayer.innerHTML = '<iframe src="http://player.vimeo.com/video/152375642" width="400" height="255" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
     container.appendChild(videoPlayer);
 
     var notification_b = document.createElement('div');
     notification_b.style['font-size'] = '11px';
     notification_b.style['padding'] = '5px';
     notification_b.innerHTML = 
+        '<p>&#x20;</p>'+
         'If you have any question, please contact me at any time, it will be my pleasure.<br><br>'+
         'Contact info is the following, <br>'+
         '<a href="javascript:contactMe()" target="_blank">av.seoul@gmail.com</a> <br>'+
@@ -71,11 +111,18 @@ var lowFrameNoti = function(){
     container.appendChild(notification_b);
 
     var close = document.createElement('div');
-    close.innerHTML = '&#x274C;';
+    close.innerHTML = '&#215;';
     close.style['cursor'] = 'pointer';
     close.style['position'] = 'absolute';
-    close.style['right'] = '0px';
-    close.style['bottom'] = '0px';
+    close.style['right'] = '5px';
+    close.style['top'] = '0px';
+    close.style['color'] = '#888';
+    close.addEventListener('mouseover', function(){
+        close.style['color'] = '#f00';
+    });
+    close.addEventListener('mouseout', function(){
+        close.style['color'] = '#888';
+    });
     close.addEventListener('click', resume);
     container.appendChild(close);
 
@@ -101,9 +148,7 @@ var checkFramedrops = function(){
     }
     
     if (fdct > 240){
-        if(fps < 25 && !fdcb){
-            audioNode.mediaElement.pause();
-            isPlaying = false;
+        if(fps < 80 && !fdcb){
             isLow = true;
             fdcb = true;
             lowFrameNoti();
@@ -598,6 +643,7 @@ document.addEventListener('DOMContentLoaded', function(){
     var init = function(){
         getTracks();
         loading();
+        preLoadlowFrameNoti();
         checkFramedrops();
         document.addEventListener('wheel', navigation, false);
     };
