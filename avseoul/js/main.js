e659bfa282da4fc540712098e5b69e3f8a57ea8a
avseoul <av.seoul@gmail.com>
2017-08-16 14:59:05 -0400

    update

---
+++ avseoul/js/main.js
@@ -0,0 +1,21 @@
+var init = function(){
+	$.getJSON("/json/content.json", function(json) {
+		var num_projects = json['projects'].length;
+		for(var i = 0; i < num_projects; i++){
+			var s = json['projects'][i];
+			var scriptNode = 
+			'<div class=\"ui_grid\" id=\'' + s['id'] + '\'>'+
+			'	<div class=\"ui_thumbnail_mask\">'+
+			'		<img class=\"ui_thumbnail\" src=\'/assets/'+ s['thumbnail_src'] + '\' onclick=\"get_work_content(\''+ i +'\')\">'+
+			'	</div>'+
+			'	<div class=\'ui_description_mask\'>'+
+			'		<div class=\"ui_subject\"><span onclick=\"get_work_content(\''+ i +'\')\">'+ s['title'] +'</span></div>'+
+			'		<div class=\"ui_description\">'+ s['description'] +'</div>'+
+			'		<div class=\'ui_date\'>'+ s['date'] +'</div>'+
+			'	</div>'+
+			'</div>';
+			$('#container').append(scriptNode);
+		}
+	});
+};
+document.addEventListener('DOMContentLoaded', init, false);
