8eaf1c24bc2e25eae8fa5c47211c41a0bcd2a6e1
avseoul <av.seoul@gmail.com>
2017-08-17 14:31:48 -0400

    website update

--- avseoul/js/main.js
+++ avseoul/js/main.js
@@ -4,19 +4,18 @@ var init = function(){
 		for(var i = 0; i < num_projects; i++){
 			var s = json['projects'][i];
 			var scriptNode = 
-			'<div class=\"ui_grid\" id=\'' + s['id'] + '\'>'+
+			'<div class=\"ui_container\" id=\'' + s['id'] + '\'>'+
 			'	<div class=\"ui_thumbnail_mask\">'+
 			'		<img class=\"ui_thumbnail\" src=\'/avseoul/assets/'+ s['thumbnail_src'] + '\' onclick=\"get_work_content(\''+ i +'\')\">'+
 			'	</div>'+
 			'	<div class=\'ui_description_mask\'>'+
-			'		<div class=\"ui_subject\"><span onclick=\"get_work_content(\''+ i +'\')\">'+ s['title'] +'</span></div>'+
-			'		<div class=\"ui_description\">'+ s['description'] +'</div>'+
-			'		<div class=\'ui_date\'>'+ s['date'] +'</div>'+
+			'		<div class=\"ui_subject\"><span onclick=\"get_work_content(\''+ i +'\')\"><av_title>'+ s['title'] +'</av_title></span></div>'+
+			'		<div class=\"ui_description\"><p>'+ s['description'] +'</p></div>'+
+			'		<div class=\'ui_date\'><p>'+ s['date'] +'</p></div>'+
 			'	</div>'+
 			'</div>';
 			$('#container').append(scriptNode);
 		}
-		console.log("working");	
 	});
 };
 document.addEventListener('DOMContentLoaded', init, false);
