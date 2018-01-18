bd1330d53718ea3424931a39b0e51939013e95c0
Sehyun Kim <av@Sehyuns-MacBook-Pro.local>
2018-01-18 11:34:20 -0500

    update css

--- avseoul/js/main.js
+++ avseoul/js/main.js
@@ -278,9 +278,9 @@ var init = function(){
 			'		<img class=\"ui_thumbnail\" src=\'/avseoul/assets/'+ s['thumbnail_src'] + '\' onclick=\"open_content(\''+ i +'\')\">'+
 			'	</div>'+
 			'	<div class=\'ui_description_mask\'>'+
-			'		<div class=\"ui_subject\"><span onclick=\"open_content(\''+ i +'\')\"><av_title>'+ s['title'] +'</av_title></span></div>'+
+			'		<div class=\"ui_subject\"><p onclick=\"open_content(\''+ i +'\')\"><av_title>'+ s['title'] +'</av_title></p></div>'+
 			'		<div class=\"ui_description\"><p>'+ s['description'] +'</p></div>'+
-			'		<div class=\"ui_url\"><div><a href=\'\'>www.avseoul.net/#'+s['id']+'</a></div></div>'+
+			'		<div class=\"ui_url\"><p><a href=\'\'>www.avseoul.net/#'+s['id']+'</a></p></div>'+
 			'		<div class=\'ui_date\'><p>'+ s['date'] +'</p></div>'+
 			'		<div class=\'ui_detail\'><a onclick=\"open_content(\''+ i +'\')\"><&#33;--details--></a></div>'+
 			'	</div>'+
