84fa6c76a41017d6cf6eb3541395fee2c54fa21a
Sehyun Av Kim <sk5642@nyu.edu>
2018-05-01 20:11:09 -0400

    delete a cinder project. have no idea why its in here

--- StrangeAttractor/xcode/build/StrangeAttractor.build/Debug/StrangeAttractor.build/Objects-normal/i386/SubMover_01.d
+++
@@ -1,9 +0,0 @@
-dependencies: \
-  /Users/avseoul/GitHub/2015.Apr_StrangeAttractor/StrangeAttractor/src/SubMover_01.cpp \
-  /Users/avseoul/GitHub/2015.Apr_StrangeAttractor/StrangeAttractor/src/SubMover_01.h \
-  ../../../../Documents/cinder_0.8.6_mac/include/cinder/Rand.h \
-  ../../../../Documents/cinder_0.8.6_mac/include/cinder/Perlin.h \
-  /Users/avseoul/GitHub/2015.Apr_StrangeAttractor/StrangeAttractor/src/SpriteImage.h \
-  ../../../../Documents/cinder_0.8.6_mac/include/cinder/gl/Texture.h \
-  /Users/avseoul/GitHub/2015.Apr_StrangeAttractor/StrangeAttractor/include/Resources.h \
-  ../../../../Documents/cinder_0.8.6_mac/include/cinder/CinderResources.h
