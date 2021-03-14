de896d9cddeaaa5a47b178ef678a51a952a4fef1
avseoul <av.seoul@gmail.com>
2021-03-13 20:47:26 -0500

    particleEqualizer is deployed

--- particleEqualizer/style.78032849.js
+++ particleEqualizer/style.78032849.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57290" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58748" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
