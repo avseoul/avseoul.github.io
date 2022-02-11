da44c939f51349adf4a6f49e8a51041b21daf82e
avseoul <av.seoul@gmail.com>
2022-02-11 15:58:41 -0500

    walk is deployed

--- walk/src/ThreeExtension/SkinToTextureMap.d.ts
+++ walk/src/ThreeExtension/SkinToTextureMap.d.ts
@@ -2,10 +2,16 @@ import { SkinnedMesh, Texture, WebGLMultipleRenderTargets } from "three";
 export default class SkinToTextureMap extends WebGLMultipleRenderTargets {
     private _source;
     private _rttHandler;
-    private _count;
-    get count(): number;
+    private _isBoneTextureAvailable;
+    private _usedSlotCount;
+    /**
+        Map size is larger than the actual vertices due to keep pow of 2 size
+    */
+    get usedSlotCount(): number;
     private _length;
     get length(): number;
+    private _size;
+    get size(): number;
     get positionTexture(): Texture;
     get normalTexture(): Texture;
     get tangentTexture(): Texture;
