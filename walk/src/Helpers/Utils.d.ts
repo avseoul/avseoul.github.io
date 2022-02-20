6c10214eb18011d3f55e485419de4c696f68567c
avseoul <av.seoul@gmail.com>
2022-02-19 21:13:17 -0500

    walk is deployed

--- walk/src/Helpers/Utils.d.ts
+++ walk/src/Helpers/Utils.d.ts
@@ -1,2 +1,3 @@
-import { Vector3 } from "three";
+import { Vector3, WebGLRenderer } from "three";
 export declare const randomSpherePoint: (radius?: number) => Vector3;
+export declare const createCubeTextureFromData: (renderer: WebGLRenderer, dataSet: ArrayBufferView[], width: number, height: number) => any;
